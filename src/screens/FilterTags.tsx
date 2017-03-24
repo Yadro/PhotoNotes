620a6422fbc9dd8ef493ad308975373ef04f04f8
yadro <yadrone@gmail.com>
2017-03-24 21:00:15 +0300

    DrawerClose, sort tags, highlight

--- src/screens/FilterTags.tsx
+++ src/screens/FilterTags.tsx
@@ -4,6 +4,8 @@ import icons from '../components/Icons';
 import {ScreenNavigationProp} from "react-navigation";
 import {connect} from "react-redux";
 import {FilterState} from "../redux/filter";
+import store from "../redux/Store";
+import {SET_CURRENT_FILTER} from "../constants/ActionTypes";
 const {editWhite} = icons;
 
 interface TagsLayerP extends ScreenNavigationProp {
@@ -16,7 +18,8 @@ interface TagsLayerS {
 class FilterTags extends React.Component<TagsLayerP, TagsLayerS> {
 
   onPress = (id) => {
-    this.props.navigation.navigate('EditFilter', {id});
+    store.dispatch({type: SET_CURRENT_FILTER, current: id});
+    this.props.navigation.navigate('DrawerClose');
   };
 
   onLongPress = id => () => {
@@ -24,10 +27,13 @@ class FilterTags extends React.Component<TagsLayerP, TagsLayerS> {
   };
 
   render() {
+    const {current} = store.getState().filter;
     const {filters} = this.props.filter;
     return <View style={{flex: 1}}>
-      {filters.map((e, idx) => <Item key={idx} title={e.title} onPress={this.onPress.bind(this, idx)}
-                                   onLongPress={this.onLongPress(idx)}/>)}
+      {filters.sort(wsort).map((e, idx) => (
+        <Item key={idx} title={e.title} selected={current == idx}
+              onPress={this.onPress.bind(this, idx)} onLongPress={this.onLongPress(idx)}/>
+      ))}
       <Item title={'Add new filter'} onPress={this.onPress.bind(this, -1)} onLongPress={this.onLongPress(-1)}/>
     </View>
   }
@@ -35,15 +41,22 @@ class FilterTags extends React.Component<TagsLayerP, TagsLayerS> {
 
 export default connect(state => ({filter: state.filter}))(FilterTags);
 
-const Item = ({title, onPress, onLongPress}) => {
-  return <TouchableNativeFeedback onPress={onPress} onLongPress={onLongPress} style={css.container}>
-    <View style={css.item}>
+const Item = ({title, onPress, onLongPress, selected}) => {
+  return <TouchableNativeFeedback onPress={onPress} onLongPress={onLongPress} delayLongPress={3000}
+                                  style={css.container}>
+    <View style={[css.item, selected && css.itemHighlight]}>
       {/*<Image source={{uri: editWhite}}/>*/}
       <Text>{title}</Text>
     </View>
   </TouchableNativeFeedback>
 };
 
+export function wsort(a, b) {
+  return sortstr(a.title, b.title);
+}
+export function sortstr(a, b) {
+  return a > b ? 1 : a == b ? 0 : -1;
+}
 
 const css = StyleSheet.create({
   container: {
@@ -52,5 +65,8 @@ const css = StyleSheet.create({
   item: {
     paddingVertical: 10,
     paddingHorizontal: 20,
+  },
+  itemHighlight: {
+    backgroundColor: 'rgba(0,0,0,.05)'
   }
 });
