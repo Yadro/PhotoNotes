9d91d4f1bb646dffadd09adb7915c8f17710375d
yadro <yadrone@gmail.com>
2017-04-01 21:13:43 +0300

    collapse navigation logger

--- index.android.js
+++ index.android.js
@@ -3,3 +3,7 @@ import {AppRegistry} from 'react-native';
 import AppWithStore from './build/src/index';
 
 AppRegistry.registerComponent('PhotoNotes', () => AppWithStore);
+
+if (console && console.group && console.groupCollapsed) {
+  console.group = console.groupCollapsed;
+}
